248f7753993a033ab93767095e8b872f7ca49799
dewei8 <1534584879@qq.com>
2018-07-17 15:39:04 +0800

    add CNPM(autoInstall)

--- utils/index.js
+++ utils/index.js
@@ -51,7 +51,7 @@ exports.runLintFix = function runLintFix(cwd, data, color) {
     )
     console.log('# ========================\n')
     const args =
-      data.autoInstall === 'npm'
+      data.autoInstall.includes('npm')
         ? ['run', 'lint', '--', '--fix']
         : ['run', 'lint', '--fix']
     return runCommand(data.autoInstall, args, {
@@ -77,7 +77,7 @@ To get started:
       data
     )}${lintMsg(data)}npm run dev`
   )}
-  
+
 Documentation can be found at https://vuejs-templates.github.io/webpack
 `
   console.log(message)
