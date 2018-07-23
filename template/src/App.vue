2c91574fea41e2eb7eb302795a6ade02d9c53472
dewei8 <1534584879@qq.com>
2018-07-23 17:57:37 +0800

    导入换肤scss函数

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <div id="app">
-    <p>loadNum:\{{loadNum}}</p>
-    <div v-if="$router.currentRoute.path!=loginRouter">
-      <router-link v-if="!$store.state.userInf" :to="loginRouter">登录</router-link>
-      <a v-else @click="toLoginOut">退出</a>
+  <div :class="skin+'-skin'">
+    <div id="app">
+      <p>loadNum:\{{loadNum}}|<span @click="tabskin">换肤</span></p>
+      <div v-if="$router.currentRoute.path!=loginRouter">
+        <router-link v-if="!$store.state.userInf" :to="loginRouter">登录</router-link>
+        <a v-else @click="toLoginOut">退出</a>
+      </div>
+      <ul>
+        <router-link tag="li" to="/Youke"><a>游客可进</a></router-link>
+        <router-link tag="li" to="/Logined"><a>登录可进</a></router-link>
+        <router-link tag="li" to="/Admin"><a>管理可进</a></router-link>
+      </ul>
+      <img src="./assets/logo.png">
+      <router-view/>
     </div>
-    <ul>
-      <router-link tag="li" to="/Youke"><a>游客可进</a></router-link>
-      <router-link tag="li" to="/Logined"><a>登录可进</a></router-link>
-      <router-link tag="li" to="/Admin"><a>管理可进</a></router-link>
-    </ul>
-    <img src="./assets/logo.png">
-    <router-view/>
   </div>
 </template>
 
@@ -23,6 +25,7 @@ export default {
   name: 'App',
   data () {
     return {
+      skin: 'blue',
       loginRouter: LOGIN_ROUTER
     }
   },
@@ -33,6 +36,9 @@ export default {
   },
   methods: {
     ...mapActions([LOGIN_OUT]),
+    tabskin () {
+      this.skin = this.skin == 'blue' ? 'purple' : 'blue'
+    },
     toLoginOut () {
       this.loginOut().then(res => {
         if (this.$router.currentRoute.matched.some(r => r.meta.loginCheck && !r.meta.loginCheck(null))) {
@@ -45,23 +51,5 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
-}
+@import './styles/index';
 </style>
